cbee90443e869f2da6376875a26234dda2833eb6
dev <dev@test.com>
2024-03-15 00:59:22 +0200

    added a seconde Dice player and change the the button

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -6,14 +6,11 @@ final randomizer = Random();
 
 class DiceRoller extends StatefulWidget {
   final int numberOfPlayers;
-  
 
   const DiceRoller({Key? key, required this.numberOfPlayers}) : super(key: key);
 
   @override
-  State<DiceRoller> createState() {
-    return _DiceRollerState();
-  }
+  State<DiceRoller> createState() => _DiceRollerState();
 }
 
 class _DiceRollerState extends State<DiceRoller> {
@@ -22,7 +19,7 @@ class _DiceRollerState extends State<DiceRoller> {
   @override
   void initState() {
     super.initState();
-    currentDiceRolls = List.generate(widget.numberOfPlayers, (index) => 2);
+    currentDiceRolls = List.generate(widget.numberOfPlayers, (index) => 1);
   }
 
   void rollDice(int playerIndex) {
@@ -32,29 +29,64 @@ class _DiceRollerState extends State<DiceRoller> {
   }
 
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        for (int i = 0; i < widget.numberOfPlayers; i++)
-          Column(
-            children: [
-              Image.asset(
-                'assets/images/dice-${currentDiceRolls[i]}.png',
-                width: 200,
-              ),
-              TextButton(
-                onPressed: () => rollDice(i),
-                style: TextButton.styleFrom(
-                  padding: const EdgeInsets.only(top: 20),
-                  foregroundColor: Colors.white,
-                  textStyle: const TextStyle(
-                    fontSize: 28,
+        if (widget.numberOfPlayers > 1)
+          Padding(
+            padding: const EdgeInsets.only(
+                bottom: 8.0), // تعديل الـ Padding للزر العلوي
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/dice-${currentDiceRolls[0]}.png',
+                  width: 200,
+                ),
+                Positioned(
+                  top: -20, // تعديل الموقع العلوي للزر ليظهر بالكامل
+                  child: Transform(
+                    alignment: Alignment.center,
+                    transform: Matrix4.rotationX(pi), // عكس الزر
+                    child: TextButton(
+                      onPressed: () => rollDice(0),
+                      style: TextButton.styleFrom(
+                        padding: const EdgeInsets.all(16.0),
+                        foregroundColor: Colors.white,
+                        textStyle: const TextStyle(
+                          fontSize: 28,
+                        ),
+                      ),
+                      child: const Text('Roll Dice'),
+                    ),
                   ),
                 ),
-                child: const Text('Roll Dice'),
-              ),
-            ],
+              ],
+            ),
+          ),
+        for (int i = 1; i < widget.numberOfPlayers; i++)
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/images/dice-${currentDiceRolls[i]}.png',
+                  width: 200,
+                ),
+                TextButton(
+                  onPressed: () => rollDice(i),
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.only(top: 10),
+                    foregroundColor: Colors.white,
+                    textStyle: const TextStyle(
+                      fontSize: 28,
+                    ),
+                  ),
+                  child: const Text('Roll Dice'),
+                ),
+              ],
+            ),
           ),
         Text(
           'Created by Eng.Abdullah',
